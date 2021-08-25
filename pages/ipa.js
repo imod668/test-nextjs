ed00a27ba5593eda59cbfa1f8820f8ec724d3233
imod668 <46364120+imod668@users.noreply.github.com>
2021-08-25 18:30:15 +0700

    second commit

--- pages/ipa.js
+++ pages/ipa.js
@@ -7,7 +7,44 @@ const Tabs = () => {
   return (
 		<Layout>
       <Head><title>IPA Page</title></Head>
-			<h1>IPA Page</h1>
+
+      <div className="app-card">
+        <div className="content-block">
+          <h4>Ipa Ios Old Version</h4>
+        </div>
+        <div className="list-block">
+
+            <div className="item-content">
+              <div className="item-media">
+                <img className="app-icon" src="/assets/images/unc0ver.png" />
+              </div>
+              <div className="item-inner">
+                <div className="item-title-row">
+                  <div className="item-title">Unc0ver_3.5.6</div>
+                  <a href="https://" target="new" className="install">GET</a>
+                </div>
+                <div className="item-subtitle">JB ios 11.0-12.2(-A12).</div>
+              </div>
+            </div>
+
+            <div className="item-content">
+              <div className="item-media">
+                <img className="app-icon" src="/assets/images/Chimera.png" />
+              </div>
+              <div className="item-inner">
+                <div className="item-title-row">
+                  <div className="item-title">Chimera v1.3.8</div>
+                  <a href="https://" target="new" className="install">GET</a>
+                </div>
+                <div className="item-subtitle">JB ios 12.0-12.1.2 all Device!</div>
+              </div>
+            </div>
+
+        </div>
+
+        <br />
+      </div>
+
 		</Layout>
     );
 }
